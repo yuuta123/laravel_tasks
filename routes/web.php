188f4c5e53dae804c1506226c3e334503117548b
yuuta.kurabuchi <yuuta-kurabuchi@ogic.co.jp>
2021-06-19 14:59:15 +0900

    folders ok

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/folders/{id}/tasks', 'TaskController@index')->name('tasks.index');
