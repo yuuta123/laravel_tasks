5589d0c656451cbc49b2723536da2bd132a53e18
yuuta.kurabuchi <yuuta-kurabuchi@ogic.co.jp>
2021-06-21 19:49:08 +0900

    タスク作成機能完了

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Folder;
 use App\Task;
 use Illuminate\Http\Request;
+use App\Http\Requests\CreateTask;
 
 class TaskController extends Controller
 {
@@ -23,6 +24,28 @@ class TaskController extends Controller
             'folders' => $folders,
             'current_folder_id' => $current_folder->id,
             'tasks' => $tasks,
+        ]);
+    }
+
+    public function showCreateForm(int $id)
+    {
+        return view('tasks/create', [
+            'folder_id' => $id,
+        ]);
+    }
+
+    public function create(int $id, CreateTask $request)
+    {
+        $current_folder = Folder::find($id);
+
+        $task = new Task();
+        $task->title = $request->title;
+        $task->due_date = $request->due_date;
+
+        $current_folder->tasks()->save($task);
+
+        return redirect()->route('tasks.index', [
+            'id' => $current_folder->id,
         ]);
     }
 }
