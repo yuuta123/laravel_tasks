5d46537c518fc2862cbb462459fa1c28b469a9ab
yuuta.kurabuchi <yuuta-kurabuchi@ogic.co.jp>
2021-06-23 19:48:18 +0900

    small fixed

--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,6 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
 Route::get('/', 'HomeController@index')->name('home');    
 
 Route::get('/folders/{id}/tasks', 'TaskController@index')->name('tasks.index');
