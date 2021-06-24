47a374a1ed12f7f421111194695e6766c582a82f
yuuta.kurabuchi <yuuta-kurabuchi@ogic.co.jp>
2021-06-24 18:51:37 +0900

    login and registar are OK.

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,84 +1,44 @@
-@extends('layouts.app')
+@extends('layout')
 
 @section('content')
     <div class="container">
-        <div class="row justify-content-center">
-            <div class="col-md-8">
-                <div class="card">
-                    <div class="card-header">{{ __('Register') }}</div>
-
-                    <div class="card-body">
-                        <form method="POST" action="{{ route('register') }}">
+        <div class="row">
+            <div class="col col-md-offset-3 col-md-6">
+                <nav class="panel panel-default">
+                    <div class="panel-heading">会員登録</div>
+                    <div class="panel-body">
+                        @if ($errors->any())
+                            <div class="alert alert-danger">
+                                @foreach ($errors->all() as $message)
+                                    <p>{{ $message }}</p>
+                                @endforeach
+                            </div>
+                        @endif
+                        <form action="{{ route('register') }}" method="POST">
                             @csrf
-
-                            <div class="form-group row">
-                                <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('Name') }}</label>
-
-                                <div class="col-md-6">
-                                    <input id="name" type="text" class="form-control @error('name') is-invalid @enderror"
-                                        name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
-
-                                    @error('name')
-                                        <span class="invalid-feedback" role="alert">
-                                            <strong>{{ $message }}</strong>
-                                        </span>
-                                    @enderror
-                                </div>
+                            <div class="form-group">
+                                <label for="email">メールアドレス</label>
+                                <input type="text" class="form-control" id="email" name="email"
+                                    value="{{ old('email') }}" />
                             </div>
-
-                            <div class="form-group row">
-                                <label for="email"
-                                    class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
-
-                                <div class="col-md-6">
-                                    <input id="email" type="email" class="form-control @error('email') is-invalid @enderror"
-                                        name="email" value="{{ old('email') }}" required autocomplete="email">
-
-                                    @error('email')
-                                        <span class="invalid-feedback" role="alert">
-                                            <strong>{{ $message }}</strong>
-                                        </span>
-                                    @enderror
-                                </div>
+                            <div class="form-group">
+                                <label for="name">ユーザー名</label>
+                                <input type="text" class="form-control" id="name" name="name" value="{{ old('name') }}" />
                             </div>
-
-                            <div class="form-group row">
-                                <label for="password"
-                                    class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
-
-                                <div class="col-md-6">
-                                    <input id="password" type="password"
-                                        class="form-control @error('password') is-invalid @enderror" name="password"
-                                        required autocomplete="new-password">
-
-                                    @error('password')
-                                        <span class="invalid-feedback" role="alert">
-                                            <strong>{{ $message }}</strong>
-                                        </span>
-                                    @enderror
-                                </div>
+                            <div class="form-group">
+                                <label for="password">パスワード</label>
+                                <input type="password" class="form-control" id="password" name="password">
                             </div>
-
-                            <div class="form-group row">
-                                <label for="password-confirm"
-                                    class="col-md-4 col-form-label text-md-right">{{ __('Confirm Password') }}</label>
-
-                                <div class="col-md-6">
-                                    <input id="password-confirm" type="password" class="form-control"
-                                        name="password_confirmation" required autocomplete="new-password">
-                                </div>
+                            <div class="form-group">
+                                <label for="password-confirm">パスワード（確認）</label>
+                                <input type="password" class="form-control" id="password-confirm" name="password_confirmation">
                             </div>
-
-                            <div class="form-group row mb-0">
-                                <div class="col-md-6 offset-md-4">
-                                    <button type="submit" class="btn btn-primary">
-                                        {{ __('Register') }}
-                                    </button>
-                                </div>
+                            <div class="text-right">
+                                <button type="submit" class="btn btn-primary">送信</button>
                             </div>
                         </form>
                     </div>
-                </div>
+                </nav>
             </div>
         </div>
     </div>
